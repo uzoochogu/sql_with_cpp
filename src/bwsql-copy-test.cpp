ca0444d0bf98d66462e1f3d359aa0badf84993cf
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-09-05 22:29:20 +0100

    ➕Added file to test bwsql wrapper on sqlite3;

---
+++ src/bwsql-copy-test.cpp
@@ -0,0 +1,18 @@
+/** 
+ * This program copies from one table to another within a db using the BWSQL wrapper 
+ * on top of sqlite3
+ * 
+ * 
+*/ 
+
+#include <BWSQL.h>
+#include <iostream>
+
+constexpr const char * db_file = DB_PATH "/scratch.db";
+
+
+int main()
+{
+
+    return 0;
+}
